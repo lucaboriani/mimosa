a1de2ddabd086ea825a2ae11d6d982151ff6c48e
zp <zeropara@gmail.com>
2022-02-18 16:58:18 +0100

    toml 2

--- src/lib/map/config.js
+++ src/lib/map/config.js
@@ -1,7 +1,6 @@
 export const variables = {
-    accessToken: import.meta.env.MODE === 'development' ? import.meta.env.VITE_MAPBOX_API_KEY : process.env.MAPBOX_API_KEY
+    accessToken: import.meta.env.MODE === 'development' ? import.meta.env.VITE_MAPBOX_API_KEY : process.env["MAPBOX_API_KEY"]
 };
-console.log(import.meta)
 
 console.log(variables)
 export const config = {
