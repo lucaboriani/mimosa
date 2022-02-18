4d1ed641b17a277976af413b0b5f745fb95e319b
zp <zeropara@gmail.com>
2022-02-18 16:59:54 +0100

    toml 3

--- src/lib/map/config.js
+++ src/lib/map/config.js
@@ -2,6 +2,7 @@ export const variables = {
     accessToken: import.meta.env.MODE === 'development' ? import.meta.env.VITE_MAPBOX_API_KEY : process.env["MAPBOX_API_KEY"]
 };
 
+console.log("variables")
 console.log(variables)
 export const config = {
     style: 'mapbox://styles/mapbox/light-v10',
