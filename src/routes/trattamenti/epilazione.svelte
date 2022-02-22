715554a79a3710c4d36e586bf468327e9c6ebcdb
zp <zeropara@gmail.com>
2022-02-22 21:10:20 +0100

    epi

--- src/routes/trattamenti/epilazione.svelte
+++ src/routes/trattamenti/epilazione.svelte
@@ -5,12 +5,13 @@
         ]
     : 
         [
-            '/static/json/epilazione.json',
+            '/json/epilazione.json',
         ] 
         
 	export async function load({ fetch }) {
 		
 		const fetchUrl = async (url) => {
+            console.log(url)
 			const response = await fetch(url);
 			const data = await response.json() 
 			return data
